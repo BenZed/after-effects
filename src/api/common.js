853387ff3ea195569c1e455c0c32d63142b58930
BenZed <bengaumond@gmail.com>
2017-07-18 20:53:39 -0700

    hmm
    Well, on windows, everything is working in theory but I can't seem to
    let my machine give Node.js write permissions. I don't know what to do
    abotu that so I'm going to com eback to it later.

--- src/api/common.js
+++ src/api/common.js
@@ -19,9 +19,9 @@ export const execPromise = (...args) => new Promise((resolve, reject) => {
   })
 })
 
-export const CMD_RES_DIR = path.join(process.cwd(), 'cmd-res-test')
+// export const CMD_RES_DIR = path.join(process.cwd(), 'cmd-res-test')
 // Switch these comments when testings
-// export const CMD_RES_DIR = os.tmpdir()
+export const CMD_RES_DIR = os.tmpdir()
 
 /******************************************************************************/
 // Errors
