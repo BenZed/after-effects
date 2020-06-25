b96505aec66450d11595454d895f6fffb64420f9
osman corluk <corluk@hotmail.com>
2020-06-25 19:05:12 +0300

    d

--- lib/includes/json.jsx
+++ lib/includes/json.jsx
@@ -1,11 +1,30 @@
-const { isGetAccessor } = require("typescript")
+ 
 
  
 
 (function (global){
 
+    var layerToJson = function (layer    ){
+        var jsonObject = {}
+        propsToJson(layer, function (key,value){
+
+            jsonObject[key] = value 
+        })
+        return jsonObject 
+        //layerProperties = layer.reflection.properties 
+
+    }
+    var methodsToJson = function (object , callback ) {
+
+
+        var methods = object.reflects.methods 
+        for(var i =0 ; i != methods.length ; i++){
+            var method = methods[i]
+            callback(object,method) 
+        }
+    }
     var propsToJson=  function (  object , fnEach ) {
-        var properties = object.reflection.properties  
+        var properties = object.reflect.properties  
         
      
         for( var i = 0 ; i != properties.length ; i++){
@@ -13,10 +32,8 @@ const { isGetAccessor } = require("typescript")
             var propertyKey  = properties[i]
 
             fnEach.call(propertyKey , object[property]) 
-            if(property == "layers"){
-                layers = layersToJson(comp)
-            }
-            jsonObject[property] = property.toString()
+            
+            
         }        
          
         return jsonObject
@@ -24,23 +41,31 @@ const { isGetAccessor } = require("typescript")
     }
 
      
-    global.comp_to_json = function (jsonObject , compItem){
+    global.comp_to_json = function (  compItem){
 
 
-    
+        var jsonObject = {}
         propsToJson (object,function(key,value){
 
             if(key == "layers"){
 
-                jsonObject.layers = layerToJson(compItem.layers())
-
+                jsonObject.layers = []
+                if(key == "layers"){ 
+                    for(var j = 1 ; j != compItem.layers.length+1  ; j++){
+                     var    layer = layerToJson(comp)
+                            jsonObject.push(layer)
+                    }
+                   
+                }
+               
             }else {
-                jsonObject[key] = value
+                jsonObject[key] = value 
             }
+                     
 
 
         }) 
-      //  methodsToJson(jsonObject,object) 
+      
         return jsonObject
     }   
 
